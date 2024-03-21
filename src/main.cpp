e1d8e883b356600e85c83f093e8e56b525e0f564
alexcoroi <a.coroi@students.htl-leonding.ac.at>
2024-03-21 12:47:14 +0100

    Included libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
 #include <Arduino.h>
-#include "web/WifiManager.h"
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println();
-        
-    // Setting the ESP as an access point
-    Serial.print("Setting AP (Access Point)…");
-    WifiManager::CreateWifiNetwork(WifiManager::SSID, WifiManager::PASSWORD);
+
 }
 
 void loop() {
-    
+  
 }
