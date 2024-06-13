3a4093bb49631b4d66ffc365751bdc97e941008c
BaysoyRabbi <145426693+BaysoyRabbi@users.noreply.github.com>
2024-06-13 11:30:10 +0200

    added buttons instead of slider in webapp

--- webapp/src/components/Icons/VibrationsensorManager.vue
+++ webapp/src/components/Icons/VibrationsensorManager.vue
@@ -1,28 +1,108 @@
 <template>
-  <div>
-    <h4>Vibrationnssensor</h4>
-    <div id="slider-control">
-      <label>{{ $t("word.sensitivity") }}</label>
-      <Slider id="slider" v-model="settings.volume"/>
+<div>
+<div id="vb">
+  <div class="label-container">
+    <label for="sensitivity">Set Sensitivity of Vibrationsensor</label>
+    <div class="button-container">
+      <Button class="button" @click="setSensitivity(25)">25%</Button>
+      <Button class="button" @click="setSensitivity(50)">50%</Button>
+      <Button class="button" @click="setSensitivity(100)">100%</Button>
     </div>
   </div>
+</div>
+<div class="current-sensitivity">
+  <span>Current Sensitivity: {{ sensitivity }}%</span>
+</div>
+<transition name="fade">
+  <div v-if="message" class="message">{{ message }}</div>
+</transition>
+</div>
 </template>
 
-<script setup>
-  import {useSettingsStore} from "@/stores/settings.js";
+<script>
+import Button from 'primevue/button';
 
-  const settings = useSettingsStore();
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      sensitivity: 50, // default sensitivity value
+      message: '',
+    };
+  },
+  methods: {
+    setSensitivity(value) {
+      this.sensitivity = value;
+      this.message = `Sensitivity set to: ${this.sensitivity}%`;
+      console.log(this.message);
+      setTimeout(() => {
+        this.message = '';
+      }, 3000);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  #slider-control {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-  }
+.label-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+  margin-top: 0.5rem;
+}
 
-  #slider {
-    margin-left: auto;
-    width: 80%;
-  }
+.button-container {
+  display: flex;
+}
+
+.button {
+  margin-left: 10px;
+}
+
+.button:first-child {
+  margin-left: 0;
+}
+
+.button:hover {
+  background-color: #45a049;
+}
+
+.current-sensitivity {
+  text-align: center;
+  margin-top: 0.2rem;
+}
+
+.current-sensitivity span {
+  margin-left: 0.1rem;
+}
+
+.message {
+  margin-top: 0.1rem;
+  padding: 0.7rem;
+  background-color: #e7f3e7;
+  color: #2c662d;
+  border: 1px solid #2c662d;
+  border-radius: 0.05rem;
+  opacity: 1;
+  transition: opacity 0.5s ease-in-out;
+}
+
+span {
+  text-align: center;
+  margin-top: 0.1rem;
+  font-weight: bold;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
--- webapp/src/views/Settings/AudioSettingsView.vue
+++ webapp/src/views/Settings/AudioSettingsView.vue
@@ -1,32 +1,73 @@
-<script setup>
-  import {useSettingsStore} from "@/stores/settings.js";
-
-  const settings = useSettingsStore();
-</script>
-
 <template>
   <div>
     <Card>
-      <template #title>{{ $t("settings.audio") }}</template>
+      <template #title>Audio</template>
       <template #content>
-        <div id="volume-slider-control">
-          <label>{{ $t("word.volume") }}</label>
-          <Slider id="volume-slider" v-model="settings.volume"/>
+        <div class="volume-slider-control">
+          <label>Volume</label>
+          <div class="button-container">
+            <Button class="volume-button" @click="setVolume(25)">25%</Button>
+            <Button class="volume-button" @click="setVolume(50)">50%</Button>
+            <Button class="volume-button" @click="setVolume(100)">100%</Button>
+          </div>
         </div>
+        <div class="current-volume">Current Volume: {{ settings.volume }}%</div>
+        <div v-if="message" class="alert-message">{{ message }}</div>
       </template>
     </Card>
   </div>
 </template>
 
-<style scoped>
-  #volume-slider-control {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-  }
+<script setup>
+import { useSettingsStore } from "@/stores/settings.js";
+import Button from "primevue/button";
 
-  #volume-slider {
-    margin-left: auto;
-    width: 80%;
-  }
+const settings = useSettingsStore();
+
+const setVolume = (value) => {
+  settings.volume = value;
+  message = `Volume set to: ${settings.volume}%`;
+  setTimeout(() => {
+    message = '';
+  }, 3000);
+};
+
+let message = '';
+</script>
+
+<style scoped>
+.volume-slider-control {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.button-container {
+  margin-left: auto;
+}
+
+.volume-button {
+  margin-left: 0.1rem;
+}
+
+.volume-button:hover {
+  background-color: #45a049;
+}
+
+.current-volume {
+  text-align: center;
+  margin-top: 0.1rem;
+  font-weight: bold;
+}
+
+.alert-message {
+  margin-top: 1rem;
+  padding: 0.7rem;
+  background-color: #e7f3e7;
+  color: #2c662d;
+  border: 1px solid #2c662d;
+  border-radius: 0.05rem;
+  opacity: 1;
+  transition: opacity 0.5s ease-in-out;
+}
 </style>
