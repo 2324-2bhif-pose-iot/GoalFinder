0f30f6f5a2a4c278c3ea7a3d84454cf54c31ae29
ssc <ssc@leonding.htl>
2024-06-19 16:23:26 +0200

    added Settings.cpp

---
+++ src/Settings.cpp
@@ -0,0 +1,47 @@
+/*
+ * ===============================================================================
+ * (c) HTL Leonding
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Licensed under MIT License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * All trademarks used in this document are property of their respective owners.
+ * ===============================================================================
+ */
+
+#include "Settings.h"
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+#include "WiFi.h"
+#include <math.h>
+
+const char* Settings::keyVolume = "volume";
+const int Settings::defaultVolume = 50;
+
+Settings::Settings() :
+    Singleton<Settings>(),
+	store()
+{
+    store.Begin("app_prefs");
+}
+
+ Settings::~Settings() {
+ }
+
+String Settings::GetMacAddress() {
+	return String(WiFi.macAddress());
+}
+
+int Settings::GetVolume() {
+	return store.GetInt(keyVolume, defaultVolume);
+}
+
+void Settings::SetVolume(int volume) {
+	volume = max(min(volume, 100), 0);
+	store.PutInt(keyVolume, volume);
+}
